5d0e909d25ab933f1b94fb5454c30122143a9789
Benjamin-Humburg <102706500+Benjamin-Humburg@users.noreply.github.com>
2022-08-30 18:43:21 -0700

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,7 @@
 # Must be a finite list.
 # The list must be an integer list.
 # Returns true if target is in list.
-# Worst case is O(n), where n is the list length
+# Worst case performance is O(n), where n is the list length
 def linear_search(list, target):
     for i in range(len(list)):
         if list[i] == target:
